9c7a873f43e00437d4b2b933ddce9e60e8487fbb
Jenny Cho <105570556+jennycho02@users.noreply.github.com>
2024-10-01 14:31:01 -0700

    fixing gradient

--- js/app.js
+++ js/app.js
@@ -1,12 +1,28 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const blob = document.getElementById("blob");
+// document.addEventListener("DOMContentLoaded", () => {
+//   const blob = document.getElementById("blob");
 
-  window.onpointermove = event => {
-    const { clientX, clientY } = event;
+//   window.onpointermove = event => {
+//     const { clientX, clientY } = event;
 
-    blob.animate({
-      left: `${clientX}px`,
-      top: `${clientY}px`
-    }, { duration: 300, fill: "forwards" });
-  };
-});
+//     blob.animate({
+//       left: `${clientX}px`,
+//       top: `${clientY}px`
+//     }, { duration: 300, fill: "forwards" });
+//   };
+// });
+
+const blob = document.getElementById("blob");
+const gradient = document.getElementById("gradient-color");
+
+window.onpointermove = event => {
+  const { clientX, clientY } = event;
+
+  // Move the blob
+  blob.animate({
+    left: `${clientX}px`,
+    top: `${clientY}px`
+  }, { duration: 300, fill: "forwards" });
+
+  // Adjust the clip-path to reveal the gradient only under the blob
+  gradient.style.clipPath = `circle(17vmax at ${clientX}px ${clientY}px)`; // 17vmax is half the blob's size
+};
