5fd371d9c2c38b80c5e8f8d39ad99f625d7d7033
Jenny Cho <105570556+jennycho02@users.noreply.github.com>
2024-10-03 20:41:31 -0700

    testing cave method

--- js/app.js
+++ js/app.js
@@ -1,12 +1,21 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const blob = document.getElementById("blob");
+// document.addEventListener("DOMContentLoaded", () => {
+//   const blob = document.getElementById("blob");
 
-  window.onpointermove = event => {
-    const { clientX, clientY } = event;
+//   window.onpointermove = event => {
+//     const { clientX, clientY } = event;
 
-    blob.animate({
-      left: `${clientX}px`,
-      top: `${clientY}px`
-    }, { duration: 2500, fill: "forwards" });
-  };
+//     blob.animate({
+//       left: `${clientX}px`,
+//       top: `${clientY}px`
+//     }, { duration: 2500, fill: "forwards" });
+//   };
+// });
+
+document.addEventListener('mousemove', function (e) {
+  const cave = document.querySelector('.cave');
+  const x = e.clientX;
+  const y = e.clientY;
+  cave.style.setProperty('--x', `${x}px`);
+  cave.style.setProperty('--y', `${y}px`);
+  cave.style.background = `radial-gradient(circle at ${x}px ${y}px, transparent 10px, rgba(0, 0, 0, 1) 100px)`;
 });
